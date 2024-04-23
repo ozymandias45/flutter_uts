8a12488e549cf86515a13cd2a25a6a692e4dc2e0
ozymandias45 <ramdann140@gmail.com>
2024-04-23 15:48:05 +0700

    refactor: Refactor fetchData method to improve readability

--- lib/pages/new-user/newsPage.dart
+++ lib/pages/new-user/newsPage.dart
@@ -20,24 +20,15 @@ class NewsList extends StatefulWidget {
 }
 
 class _NewsListState extends State<NewsList> {
-  List<dynamic> _articles = [];
-
-  @override
-  void initState() {
-    super.initState();
-    fetchData();
-  }
-
-  Future<void> fetchData() async {
+  Future<List<dynamic>> _fetchData() async {
     final response = await http.get(Uri.parse(
         'https://newsapi.org/v2/everything?q=apple&from=2024-04-22&to=2024-04-22&sortBy=popularity&apiKey=cc6b8f1e9acb4b54a5f799532d900404'));
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
       final List<dynamic> articles = data['articles'] ?? [];
-      setState(() {
-        _articles =
-            articles.where((article) => article['urlToImage'] != null).toList();
-      });
+      return articles
+          .where((article) => article['urlToImage'] != null)
+          .toList();
     } else {
       throw Exception('Failed to load data');
     }
@@ -45,38 +36,56 @@ class _NewsListState extends State<NewsList> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: _articles.length,
-      itemBuilder: (context, index) {
-        final article = _articles[index];
-        return ListTile(
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 8),
-              Text(
-                article['title'] ?? 'No Title',
-                style: const TextStyle(
-                  fontWeight:
-                      FontWeight.bold,
-                  fontSize: 18,
+    return FutureBuilder<List<dynamic>>(
+      future: _fetchData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.black54,
+              valueColor: AlwaysStoppedAnimation<Color>(Colors
+                  .blue), // Berikan warna biru pada CircularProgressIndicator
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Text('Error: ${snapshot.error}'),
+          );
+        } else {
+          final List<dynamic> articles = snapshot.data ?? [];
+          return ListView.builder(
+            itemCount: articles.length,
+            itemBuilder: (context, index) {
+              final article = articles[index];
+              return ListTile(
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 8),
+                    Text(
+                      article['title'] ?? 'No Title',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    if (article['urlToImage'] != null)
+                      Container(
+                        width: double.infinity,
+                        height: 200,
+                        child: Image.network(
+                          article['urlToImage'],
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    const SizedBox(height: 4),
+                    Text(article['description'] ?? 'No Description'),
+                  ],
                 ),
-              ),
-              if (article['urlToImage'] != null)
-                Container(
-                  width: double.infinity,
-                  height: 200,
-                  child: Image.network(
-                    article['urlToImage'],
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              const SizedBox(
-                  height: 4),
-              Text(article['description'] ?? 'No Description'),
-            ],
-          ),
-        );
+              );
+            },
+          );
+        }
       },
     );
   }
