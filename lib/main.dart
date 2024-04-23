a4372db3945aa16b69614d72c6bf82edb62d79cb
ozymandias45 <ramdann140@gmail.com>
2024-04-23 15:25:40 +0700

    refactor: Update news API endpoint and filter articles by image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/new-user/firstPage.dart';
+import 'pages/new-user/newsPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ramdan Nugraha',
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
       debugShowCheckedModeBanner: false,
-      home: const FirstPage(),
+      home: NewsPage(),
     );
   }
 }
--- lib/pages/new-user/newsPage.dart
+++ lib/pages/new-user/newsPage.dart
@@ -30,11 +30,13 @@ class _NewsListState extends State<NewsList> {
 
   Future<void> fetchData() async {
     final response = await http.get(Uri.parse(
-        'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=cc6b8f1e9acb4b54a5f799532d900404'));
+        'https://newsapi.org/v2/everything?q=apple&from=2024-04-22&to=2024-04-22&sortBy=popularity&apiKey=cc6b8f1e9acb4b54a5f799532d900404'));
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
+      final List<dynamic> articles = data['articles'] ?? [];
       setState(() {
-        _articles = data['articles'];
+        _articles =
+            articles.where((article) => article['urlToImage'] != null).toList();
       });
     } else {
       throw Exception('Failed to load data');
@@ -48,8 +50,32 @@ class _NewsListState extends State<NewsList> {
       itemBuilder: (context, index) {
         final article = _articles[index];
         return ListTile(
-          title: Text(article['title']),
-          subtitle: Text(article['description']),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 8),
+              Text(
+                article['title'] ?? 'No Title',
+                style: const TextStyle(
+                  fontWeight:
+                      FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              if (article['urlToImage'] != null)
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  child: Image.network(
+                    article['urlToImage'],
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              const SizedBox(
+                  height: 4),
+              Text(article['description'] ?? 'No Description'),
+            ],
+          ),
         );
       },
     );
